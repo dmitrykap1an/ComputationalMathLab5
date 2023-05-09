2b013a61876889b9f9944dbebcb40260f65537ed
dmitrykap1an <dk475641@gmail.com>
2023-05-09 15:31:32 +0300

    Init Commit

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation ("org.jetbrains.lets-plot:lets-plot-batik:3.0.0")
+    implementation ("org.jetbrains.lets-plot:lets-plot-kotlin-jvm:4.2.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+application {
+    mainClass.set("MainKt")
+}
